d63b99f2ec8439786083183d20e4516c1493ef51
Zack Creach <zackcreach@gmail.com>
2018-04-08 12:25:15 -0400

    Completed instagram module.

---
+++ assets/scripts/modules/instagram.js
@@ -0,0 +1,49 @@
+console.log("Starting instagram..");
+
+const instagramContainer = document.querySelector(".instagram__container");
+const fetchData = (url) => {
+  return fetch(url)
+    .then((data) => data.json())
+    .then((json) => {
+      if (json) {
+        return Promise.resolve(json);
+      } else {
+        return Promise.reject(Error("json is undefined!"));
+      }
+    });
+};
+const addPost = (container, src, href, caption) => {
+  const link = document.createElement("a");
+  link.classList.add("instagram__link");
+  link.setAttribute("href", href);
+  link.setAttribute("target", "_blank");
+
+  const post = document.createElement("img");
+  post.classList.add("instagram__image");
+  post.setAttribute("src", src);
+  post.setAttribute("alt", caption);
+
+  link.appendChild(post);
+  container.appendChild(link);
+};
+const createPosts = ({ data }) => {
+  console.log(data);
+  instagramContainer.textContent = "";
+  data.map((post, index) => {
+    if (index <= 4) {
+      addPost(
+        instagramContainer,
+        post.images.thumbnail.url,
+        post.link,
+        post.caption.text,
+      );
+    }
+  });
+};
+
+const base = "https://api.instagram.com/v1/users/self/media/recent/";
+const token = "7287561625.27aa922.66b1900e83104eafb449d1081fdc03b6";
+
+fetchData(`${base}?access_token=${token}`)
+  .then((data) => createPosts(data))
+  .catch((error) => console.error(error));
--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,9 +1,8 @@
-import Menu from "./modules/menu.js";
-import EmailForm from "./modules/emailForm.js";
-import Navigation from "./modules/navigation.js";
+// import Menu from "./modules/menu.js";
+// import EmailForm from "./modules/emailForm.js";
+// import Navigation from "./modules/navigation.js";
+import "./modules/instagram.js";
 
-console.log("Hello from The Discoverer!");
-
-const menu = new Menu();
-const emailForm = new EmailForm();
-const navigation = new Navigation();
+// const menu = new Menu();
+// const emailForm = new EmailForm();
+// const navigation = new Navigation();
